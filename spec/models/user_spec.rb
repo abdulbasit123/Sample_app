765c297c56e2ee139b6aec5e8ee648d2452b432d
Haseeb Khan <haseeb@reelgeniefilms.com>
2013-07-22 00:17:52 -0700

    Signed-off-by: Haseeb Khan <haseeb@reelgeniefilms.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
+  
   it { should be_valid }
 
   describe "when name is not present" do
@@ -100,6 +101,10 @@ describe "when password is not present" do
 end
 
 
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 
 end
